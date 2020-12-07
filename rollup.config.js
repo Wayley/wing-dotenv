d98400f2b9ea52f143601778253878fc1afc65cf
wangzheng <zheng.wang@fih-foxconn.com>
2020-12-07 09:58:51 +0800

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { terser } from 'rollup-plugin-terser';
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/wing-dotenv.js',
+      name: 'wing-dotenv',
+      format: 'umd',
+    },
+    {
+      file: 'dist/wing-dotenv.es.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/wing-dotenv.amd.js',
+      format: 'amd',
+    },
+    {
+      file: 'dist/wing-dotenv.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [terser()],
+};
